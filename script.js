7306883f1c61045c80085e897d73df5897e2be34
Elizabeth <tjeudy.e@gmail.com>
2025-05-29 23:47:31 -0400

    lottie animation

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
 	const dateDiv = document.getElementById("currentDate");
+	const today = new Date();
+
 	if (dateDiv) {
 		dateDiv.textContent = `Today's date is: ${today.toDateString()}`;
 	}
 
-	console.log("Loaded Succuessfully");
+	console.log("Loaded Successfully");
 });
+
+function playSound() {
+	const sound = document.getElementById("woosh-sound");
+	sound.currentTime = 0;
+	sound.play();
+}
